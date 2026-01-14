58985c404f48f059376445bb8874588844333734
Youseef0123 <youseefmahmoudahmed212105084@gmail.com>
2026-01-15 01:30:41 +0200

    ..

--- db/connection.js
+++ db/connection.js
@@ -8,11 +8,6 @@ export const sequelize = new Sequelize(
     host: process.env.DB_HOST,
     dialect: "mysql",
     logging: false,
-    dialectOptions: {
-      ssl: {
-        require: true,
-      },
-    },
   }
 );
 
